4e5ac6e2ed841760f31c9c4042c691789d31810c
Karabosithole <sitholekarabo0@gmail.com>
2024-07-17 14:50:00 +0100

    for loop and while loop practice

--- basics/even or odd/even_or_odd.py
+++ basics/even or odd/even_or_odd.py
@@ -1,3 +1,18 @@
-"""
-Write a function that determines whether a function is even or odd
-"""
+def is_even_or_odd(number):
+    """
+    Determines whether a number is even or odd.
+
+    Parameters:
+    number (int): The number to check.
+
+    Returns:
+    str: "Even" if the number is even, "Odd" if the number is odd.
+    """
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+print(is_even_or_odd(4))  # Output: "Even"
+print(is_even_or_odd(7))  # Output: "Odd"
